7dfc831d8d0dab59fd18c6d3faf973776853b87b
sannaga3 <n.tetsuya02@gmail.com>
2021-02-25 21:37:38 +0900

    Postモデル、postsコントローラ作成

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'post/index'
+  get 'post/new'
+  get 'post/create'
+  get 'post/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
