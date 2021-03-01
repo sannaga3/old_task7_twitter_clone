1df744716bd8bcc495d36f6d1b4464c0eba22624
sannaga3 <n.tetsuya02@gmail.com>
2021-03-02 01:12:10 +0900

    createアクション作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,10 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "ささやき完了"
+    end
   end
 
   def edit
